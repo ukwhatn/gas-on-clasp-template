1817eb9514a08129ce0544f9859a443cdbb2cac9
Yuki Watanabe <ukwhatn@gmail.com>
2025-12-20 11:54:24 +0900

    refactor: src/index.tsの型安全性を改善
    - any型キャストを削除
    - exportを使用したグローバル関数エクスポートに変更
    - JSDoc追加
    - gas-webpack-pluginがexportをグローバル関数に変換

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-(global as any).main = () => {
-  console.log('main')
+/**
+ * @fileoverview エントリーポイント
+ * GASグローバル関数として公開する関数をエクスポート
+ * gas-webpack-pluginがexportされた関数をグローバル関数に変換する
+ */
+
+/**
+ * メイン関数
+ * GASから直接呼び出される
+ */
+export function main(): void {
+  console.log('main');
 }
